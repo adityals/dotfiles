e258ab7365d1441c9c7445f47924933b55cacf51
ByteDance <aditya.septiadi@bytedance.com>
2025-01-11 05:14:48 +0700

    chore: inline diagnostic

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -17,11 +17,6 @@ return {
       overrides = function(colors)
         local theme = colors.theme
 
-        local makeDiagnosticColor = function(color)
-          local c = require 'kanagawa.lib.color'
-          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
-        end
-
         return {
           -- https://github.com/rebelot/kanagawa.nvim?tab=readme-ov-file#dark-completion-popup-menu
           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
@@ -41,12 +36,6 @@ return {
           -- set their background accordingly if you wish to keep them dark and borderless
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-          -- https://github.com/rebelot/kanagawa.nvim/blob/master/README.md#tint-background-of-diagnostic-messages-with-their-foreground-color
-          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
-          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
-          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
-          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
         }
       end,
     }
