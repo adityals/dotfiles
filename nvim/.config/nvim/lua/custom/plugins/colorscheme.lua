86f4af17213f1558c1b458ce0fb8639ea468834e
ByteDance <aditya.septiadi@bytedance.com>
2025-03-29 12:42:23 +0700

    refactor: lsp nvim 0.11

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,19 @@
 return {
   {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+      }
+      vim.cmd 'colorscheme rose-pine-moon'
+    end,
+  },
+  {
     'aliqyan-21/darkvoid.nvim',
     enabled = false,
     lazy = false,
@@ -35,49 +49,5 @@ return {
       }
       vim.cmd 'colorscheme darkvoid'
     end,
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    name = 'kanagawa',
-    priority = 1000,
-    config = function()
-      require('kanagawa').setup {
-        -- https://github.com/rebelot/kanagawa.nvim/blob/master/README.md#remove-gutter-background
-        transparent = true,
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = 'none',
-              },
-            },
-          },
-        },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            -- https://github.com/rebelot/kanagawa.nvim?tab=readme-ov-file#dark-completion-popup-menu
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-            PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-
-            -- https://github.com/rebelot/kanagawa.nvim/blob/master/README.md#transparent-floating-windows
-            NormalFloat = { bg = 'none' },
-            FloatBorder = { bg = 'none' },
-            FloatTitle = { bg = 'none' },
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-            -- Popular plugins that open floats will link to NormalFloat by default;
-            -- set their background accordingly if you wish to keep them dark and borderless
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          }
-        end,
-      }
-      vim.cmd 'colorscheme kanagawa-dragon'
-    end,
-  },
+  }
 }
---
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -0,0 +1,33 @@
+-- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/configs
+vim.lsp.enable({
+    'lua_ls',
+    'vtsls',
+    'eslint',
+    'gopls',
+})
+
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true
+    },
+    virtual_text = false,
+    severity_sort = true,
+})
+
+--  merge lsp capabilities with blink (blink should not be lazy)
+local capabilities = {
+    textDocument = {
+        semanticTokens = {
+            multilineTokenSupport = true,
+        },
+        foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
+    }
+}
+capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+vim.lsp.config('*', {
+    capabilities = capabilities,
+    root_markers = { '.git' }
+})
