b0cc7650dd7e7752ad132567c872680577f12b63
Aditya Septiadi <aditya.septiadi@bytedance.com>
2025-05-09 09:24:43 +0700

    chore: theme kanso

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
     {
-        'projekt0n/github-nvim-theme',
-        name = 'github-theme',
+        "webhooked/kanso.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            require('github-theme').setup({})
-            vim.cmd('colorscheme github_dark_default')
+            vim.cmd('colorscheme kanso-zen')
         end,
     },
     {
