4788e98f38c8f389b17e4be66678c1ea959bbedd
Aditya Septiadi <aditya.septiadi@bytedance.com>
2026-05-18 07:46:52 +0700

    chore: catpuccin

--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -1,4 +1,4 @@
-vim.cmd("colorscheme wildcharm")
+vim.cmd 'colorscheme catppuccin'
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
