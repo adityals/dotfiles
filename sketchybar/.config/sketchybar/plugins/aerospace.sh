55294d9610ead772c20838afa1e881d1d6ebcc9c
Aditya Septiadi <aditseptiadi@gmail.com>
2026-09-24 10:28:52 +0700

    chore: fresh style sketchybarrc

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+FOCUSED_WORKSPACE="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME label.color=0xff979fec
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME label.color=0x50979fec
 fi
